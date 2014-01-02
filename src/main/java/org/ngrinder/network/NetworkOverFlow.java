0a11ef850428ed286a3ece32521f807773a23537
junoyoon <junoyoon@nhn.com>
2014-01-02 13:34:04 +0900

    Make network overflow plugin compatible with 3.3

--- src/main/java/org/ngrinder/network/NetworkOverFlow.java
+++ src/main/java/org/ngrinder/network/NetworkOverFlow.java
@@ -1,15 +1,10 @@
 package org.ngrinder.network;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import net.grinder.common.processidentity.AgentIdentity;
 import net.grinder.statistics.ImmutableStatisticsSet;
 import net.grinder.statistics.StatisticsIndexMap;
 import net.grinder.statistics.StatisticsIndexMap.LongIndex;
-import net.grinder.util.UnitUtil;
-
+import net.grinder.util.UnitUtils;
 import org.apache.commons.lang.StringUtils;
 import org.ngrinder.extension.OnTestSamplingRunnable;
 import org.ngrinder.model.AgentInfo;
@@ -22,100 +17,99 @@ import org.ngrinder.service.ISingleConsole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 /**
- * Network overflow plugin. This plugin blocks test running which causes the network overflow by the
- * large test.
- * 
+ * Network overflow plugin.
+ * This plugin blocks test running which causes the network overflow by the large test.
+ *
  * @author JunHo Yoon
- * @since 3.1.2
- * 
+ * @since 3.3
  */
 public class NetworkOverFlow implements OnTestSamplingRunnable {
-	private static final Logger LOGGER = LoggerFactory.getLogger(NetworkOverFlow.class);
-	private final IConfig config;
-	private IAgentManagerService agentManagerService;
-	private Map<Long, Long> limitMap = new ConcurrentHashMap<Long, Long>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(NetworkOverFlow.class);
+    private final IConfig config;
+    private final IAgentManagerService agentManagerService;
+    private long limit;
 
-	public NetworkOverFlow(IConfig config, IAgentManagerService agentManagerService) {
-		this.config = config;
-		this.agentManagerService = agentManagerService;
-	}
+    private static final String PROP_NETWORK_OVERFLOW_PERTEST_LIMIT = "plugin.networkoverflow.pertest.limit";
+    private static final int PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT = 128;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ngrinder.extension.OnTestSamplingRunnable#startSampling(org.ngrinder.service.ISingleConsole
-	 * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
-	 */
-	@Override
-	public void startSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
-		List<AgentIdentity> allAttachedAgents = singleConsole.getAllAttachedAgents();
-		int consolePort = singleConsole.getConsolePort();
-		int userSpecificAgentCount = 0;
-		for (AgentInfo each : getLocalAgents()) {
-			if (each.getPort() == consolePort && StringUtils.contains(each.getRegion(), "owned")) {
-				userSpecificAgentCount++;
-			}
-		}
-		long configuredLimit = getLimit();
-		int totalAgentSize = allAttachedAgents.size();
-		int sharedAgent = (totalAgentSize - userSpecificAgentCount);
-		long realLimit = sharedAgent == 0 ? Long.MAX_VALUE
-						: (long) (configuredLimit / (((float) sharedAgent) / totalAgentSize));
-		limitMap.put(perfTest.getId(), realLimit);
+    public NetworkOverFlow(IConfig config, IAgentManagerService agentManagerService) {
+        this.config = config;
+        this.agentManagerService = agentManagerService;
+    }
 
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.ngrinder.extension.OnTestSamplingRunnable#startSampling(org.ngrinder.service.ISingleConsole
+     * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
+     */
+    @Override
+    public void startSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
+        List<AgentIdentity> allAttachedAgents = singleConsole.getAllAttachedAgents();
+        int consolePort = singleConsole.getConsolePort();
+        int userSpecificAgentCount = 0;
+        for (AgentInfo each : getLocalAgents()) {
+            if (each.getPort() == consolePort && StringUtils.contains(each.getRegion(), "owned")) {
+                userSpecificAgentCount++;
+            }
+        }
+        long configuredLimit = getLimit();
+        int totalAgentSize = allAttachedAgents.size();
+        int sharedAgent = (totalAgentSize - userSpecificAgentCount);
+        limit = sharedAgent == 0 ? Long.MAX_VALUE
+                : (long) (configuredLimit / (((float) sharedAgent) / totalAgentSize));
 
-	protected int getLimit() {
-		return this.config.getSystemProperties().getPropertyInt("ngrinder.pertest.bandwidth.limit.megabyte", 128) * 1024 * 1024;
-	}
+    }
 
-	protected List<AgentInfo> getLocalAgents() {
-		return agentManagerService.getLocalAgents();
-	}
+    int getLimit() {
+        return this.config.getControllerProperties().getPropertyInt(PROP_NETWORK_OVERFLOW_PERTEST_LIMIT, PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT) * 1024 * 1024;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ngrinder.extension.OnTestSamplingRunnable#sampling(org.ngrinder.service.ISingleConsole,
-	 * org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService,
-	 * net.grinder.statistics.ImmutableStatisticsSet, net.grinder.statistics.ImmutableStatisticsSet)
-	 */
-	@Override
-	public void sampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService,
-					ImmutableStatisticsSet intervalStatistics, ImmutableStatisticsSet cumulativeStatistics) {
-		LongIndex longIndex = singleConsole.getStatisticsIndexMap().getLongIndex(
-						StatisticsIndexMap.HTTP_PLUGIN_RESPONSE_LENGTH_KEY);
-		Long byteSize = intervalStatistics.getValue(longIndex);
-		if (byteSize != null) {
-			Long limit = limitMap.get(perfTest.getId());
-			if (byteSize != null && byteSize > limit) {
-				if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
-					String message = String.format("TOO MUCH TRAFFIC by this test. STOP BY FORCE.\n"
-									+ "- LIMIT : %s - SENT :%s", UnitUtil.byteCountToDisplaySize(limit),
-									UnitUtil.byteCountToDisplaySize(byteSize));
-					LOGGER.info(message);
-					LOGGER.info("Forcely Stop the test {}", perfTest.getTestIdentifier());
-					perfTestService.markStatusAndProgress(perfTest, Status.ABNORMAL_TESTING, message);
-				}
-				return;
-			}
+    List<AgentInfo> getLocalAgents() {
+        return agentManagerService.getLocalAgents();
+    }
 
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.ngrinder.extension.OnTestSamplingRunnable#sampling(org.ngrinder.service.ISingleConsole,
+     * org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService,
+     * net.grinder.statistics.ImmutableStatisticsSet, net.grinder.statistics.ImmutableStatisticsSet)
+     */
+    @Override
+    public void sampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService,
+                         ImmutableStatisticsSet intervalStatistics, ImmutableStatisticsSet cumulativeStatistics) {
+        LongIndex longIndex = singleConsole.getStatisticsIndexMap().getLongIndex(
+                StatisticsIndexMap.HTTP_PLUGIN_RESPONSE_LENGTH_KEY);
+        Long byteSize = intervalStatistics.getValue(longIndex);
+        if (byteSize > this.limit) {
+            if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
+                String message = String.format("Too much traffic on this test. Stop by force.\n"
+                        + "- LIMIT : %s - SENT :%s", UnitUtils.byteCountToDisplaySize(this.limit),
+                        UnitUtils.byteCountToDisplaySize(byteSize));
+                LOGGER.info(message);
+                LOGGER.info("Stop the test {} by force", perfTest.getTestIdentifier());
+                perfTestService.markStatusAndProgress(perfTest, Status.ABNORMAL_TESTING, message);
+            }
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ngrinder.extension.OnTestSamplingRunnable#endSampling(org.ngrinder.service.ISingleConsole
-	 * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
-	 */
-	@Override
-	public void endSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
-		limitMap.remove(perfTest.getId());
-	}
+
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.ngrinder.extension.OnTestSamplingRunnable#endSampling(org.ngrinder.service.ISingleConsole
+     * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
+     */
+    @Override
+    public void endSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
+    }
 
 }
--- src/main/java/org/ngrinder/network/PhysicalNetworkOverFlow.java
+++ src/main/java/org/ngrinder/network/PhysicalNetworkOverFlow.java
@@ -1,11 +1,8 @@
 package org.ngrinder.network;
 
-import java.util.Set;
-
 import net.grinder.console.communication.AgentProcessControlImplementation.AgentStatus;
 import net.grinder.engine.controller.AgentControllerIdentityImplementation;
-import net.grinder.util.UnitUtil;
-
+import net.grinder.util.UnitUtils;
 import org.apache.commons.lang.StringUtils;
 import org.ngrinder.extension.OnPeriodicWorkingAgentCheckRunnable;
 import org.ngrinder.model.PerfTest;
@@ -15,57 +12,61 @@ import org.ngrinder.service.IPerfTestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Set;
+
 /**
  * Physical Network overflow blocking plugin. This plugin blocks tests which cause very large amount
  * of physical traffic.
- * 
+ *
  * @author JunHo Yoon
- * @since 3.1.2
+ * @since 3.3
  */
 public class PhysicalNetworkOverFlow implements OnPeriodicWorkingAgentCheckRunnable {
-	private static final Logger LOGGER = LoggerFactory.getLogger(PhysicalNetworkOverFlow.class);
-	private final IConfig config;
-	private final IPerfTestService perfTestService;
-	public static final String NGRINDER_PROP_BANDWIDTH_LIMIT_MEGABYTE = "ngrinder.bandwidth.limit.megabyte";
-	public static final int NGRINDER_PROP_BANDWIDTH_LIMIT_MEGABYTE_DEFAULT_VALUE = 128;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhysicalNetworkOverFlow.class);
+    private final IConfig config;
+    private final IPerfTestService perfTestService;
+    private static final String PROP_NETWORK_OVERFLOW_LIMIT = "plugin.networkoverflow.limit";
+    private static final int PROP_NETWORK_OVERFLOW_LIMIT_DEFAULT = 128;
 
-	public PhysicalNetworkOverFlow(IConfig config, IPerfTestService perfTestService) {
-		this.config = config;
-		this.perfTestService = perfTestService;
-	}
+    public PhysicalNetworkOverFlow(IConfig config, IPerfTestService perfTestService) {
+        this.config = config;
+        this.perfTestService = perfTestService;
+    }
 
-	@Override
-	public void checkWorkingAgent(Set<AgentStatus> workingAgents) {
-		int totalRecieved = 0;
-		int totalSent = 0;
-		if (workingAgents.isEmpty()) {
-			return;
-		}
+    @Override
+    public void checkWorkingAgent(Set<AgentStatus> workingAgents) {
+        int totalReceived = 0;
+        int totalSent = 0;
+        if (workingAgents.isEmpty()) {
+            return;
+        }
+        String region = "";
+        for (AgentStatus each : workingAgents) {
+            final String eachAgentRegion = ((AgentControllerIdentityImplementation) each.getAgentIdentity()).getRegion();
+            if (!StringUtils.contains(eachAgentRegion, "owned_")) {
+                region = eachAgentRegion;
+                totalReceived += each.getSystemDataModel().getReceivedPerSec();
+                totalSent += each.getSystemDataModel().getSentPerSec();
+            }
+        }
 
-		for (AgentStatus each : workingAgents) {
-			if (!StringUtils.contains(((AgentControllerIdentityImplementation) each.getAgentIdentity()).getRegion(),
-							"owned_")) {
-				totalRecieved += each.getSystemDataModel().getRecievedPerSec();
-				totalSent += each.getSystemDataModel().getSentPerSec();
-			}
-		}
-
-		int limit = config.getSystemProperties().getPropertyInt(NGRINDER_PROP_BANDWIDTH_LIMIT_MEGABYTE,
-						NGRINDER_PROP_BANDWIDTH_LIMIT_MEGABYTE_DEFAULT_VALUE) * 1024 * 1024;
-		if (totalRecieved > limit || totalSent > limit) {
-			LOGGER.debug("LIMIT : {}, RX : {}, TX : {}", new Object[] { limit, totalRecieved, totalSent });
-			for (PerfTest perfTest : perfTestService.getTestingPerfTest()) {
-				if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
-					perfTestService.markStatusAndProgress(
-									perfTest,
-									Status.ABNORMAL_TESTING,
-									String.format("TOO MUCH TRAFFIC on this region. STOP BY FORCE.\n"
-													+ "- LIMIT/s: %s\n" + "- RX/s: %s / TX/s: %s",
-													UnitUtil.byteCountToDisplaySize(limit),
-													UnitUtil.byteCountToDisplaySize(totalRecieved),
-													UnitUtil.byteCountToDisplaySize(totalSent)));
-				}
-			}
-		}
-	}
+        int limit = config.getControllerProperties().getPropertyInt(PROP_NETWORK_OVERFLOW_LIMIT,
+                PROP_NETWORK_OVERFLOW_LIMIT_DEFAULT) * 1024 * 1024;
+        if (totalReceived > limit || totalSent > limit) {
+            LOGGER.debug("LIMIT : {}, RX : {}, TX : {}", new Object[]{limit, totalReceived, totalSent});
+            for (PerfTest perfTest : perfTestService.getAllTesting()) {
+                if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
+                    perfTestService.markStatusAndProgress(
+                            perfTest,
+                            Status.ABNORMAL_TESTING,
+                            String.format("Too much traffic on the region %s. Stop by force.\n"
+                                    + "- LIMIT/s: %s\n" + "- RX/s: %s / TX/s: %s",
+                                    region,
+                                    UnitUtils.byteCountToDisplaySize(limit),
+                                    UnitUtils.byteCountToDisplaySize(totalReceived),
+                                    UnitUtils.byteCountToDisplaySize(totalSent)));
+                }
+            }
+        }
+    }
 }
