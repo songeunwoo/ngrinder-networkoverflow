a37b0aeefa274fb4fb051849d3555b16fd580757
junoyoon <junoyoon@gmail.com>
2016-03-18 15:01:56 +0900

    #15 Plugin Framework for Java (PF4J) - network overflow
    - pom.xml add pf4j-spring
    - NetworkOverFlow pf4j
    - PhysicalNetworkOverFlow  pf4j
    - readme build
    
    Signed-off-by: geunwoo.son <songeunwoo@nhn.com>

--- src/main/java/org/ngrinder/network/NetworkOverFlow.java
+++ src/main/java/org/ngrinder/network/NetworkOverFlow.java
@@ -1,10 +1,13 @@
 package org.ngrinder.network;
 
+import java.util.List;
+
 import net.grinder.common.processidentity.AgentIdentity;
 import net.grinder.statistics.ImmutableStatisticsSet;
 import net.grinder.statistics.StatisticsIndexMap;
 import net.grinder.statistics.StatisticsIndexMap.LongIndex;
 import net.grinder.util.UnitUtils;
+
 import org.apache.commons.lang.StringUtils;
 import org.ngrinder.extension.OnTestSamplingRunnable;
 import org.ngrinder.model.AgentInfo;
@@ -16,8 +19,12 @@ import org.ngrinder.service.IPerfTestService;
 import org.ngrinder.service.ISingleConsole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
-import java.util.List;
+import ro.fortsoft.pf4j.Extension;
+import ro.fortsoft.pf4j.PluginWrapper;
+import ro.fortsoft.pf4j.spring.SpringPlugin;
 
 /**
  * Network overflow plugin.
@@ -26,90 +33,108 @@ import java.util.List;
  * @author JunHo Yoon
  * @since 3.3
  */
-public class NetworkOverFlow implements OnTestSamplingRunnable {
-    private static final Logger LOGGER = LoggerFactory.getLogger(NetworkOverFlow.class);
-    private final IConfig config;
-    private final IAgentManagerService agentManagerService;
-    private long limit;
+public class NetworkOverFlow extends SpringPlugin {
+	private static final Logger LOGGER = LoggerFactory.getLogger(NetworkOverFlow.class);
 
-    private static final String PROP_NETWORK_OVERFLOW_PERTEST_LIMIT = "plugin.networkoverflow.pertest.limit";
-    private static final int PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT = 128;
+	public NetworkOverFlow(PluginWrapper wrapper) {
+		super(wrapper);
+	}
 
-    public NetworkOverFlow(IConfig config, IAgentManagerService agentManagerService) {
-        this.config = config;
-        this.agentManagerService = agentManagerService;
-    }
+	@Override
+	protected ApplicationContext createApplicationContext() {
+		return null;
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.ngrinder.extension.OnTestSamplingRunnable#startSampling(org.ngrinder.service.ISingleConsole
-     * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
-     */
-    @Override
-    public void startSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
-        List<AgentIdentity> allAttachedAgents = singleConsole.getAllAttachedAgents();
-        int consolePort = singleConsole.getConsolePort();
-        int userSpecificAgentCount = 0;
-        for (AgentInfo each : getLocalAgents()) {
-            if (each.getPort() == consolePort && StringUtils.contains(each.getRegion(), "owned")) {
-                userSpecificAgentCount++;
-            }
-        }
-        long configuredLimit = getLimit();
-        int totalAgentSize = allAttachedAgents.size();
-        int sharedAgent = (totalAgentSize - userSpecificAgentCount);
-        limit = sharedAgent == 0 ? Long.MAX_VALUE
-                : (long) (configuredLimit / (((float) sharedAgent) / totalAgentSize));
+	@Extension
+	public static class NetworkOverFlowExtension implements OnTestSamplingRunnable {
+		private long limit;
+		private static final String PROP_NETWORK_OVERFLOW_PERTEST_LIMIT = "plugin.networkoverflow.pertest.limit";
+		private static final int PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT = 128;
 
-    }
+		@Autowired
+		private IConfig config;
 
-    int getLimit() {
-        return this.config.getControllerProperties().getPropertyInt(PROP_NETWORK_OVERFLOW_PERTEST_LIMIT, PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT) * 1024 * 1024;
-    }
+		@Autowired
+		private IAgentManagerService agentManagerService;
 
-    List<AgentInfo> getLocalAgents() {
-        return agentManagerService.getLocalAgents();
-    }
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see
+		 * org.ngrinder.extension.OnTestSamplingRunnable#startSampling(org.ngrinder.service.ISingleConsole
+		 * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
+		 */
+		@Override
+		public void startSampling(ISingleConsole singleConsole, PerfTest perfTest,
+			IPerfTestService perfTestService) {
+			List<AgentIdentity> allAttachedAgents = singleConsole.getAllAttachedAgents();
+			int consolePort = singleConsole.getConsolePort();
+			int userSpecificAgentCount = 0;
+			for (AgentInfo each : getLocalAgents()) {
+				if (each.getPort() == consolePort
+					&& StringUtils.contains(each.getRegion(), "owned")) {
+					userSpecificAgentCount++;
+				}
+			}
+			long configuredLimit = getLimit();
+			int totalAgentSize = allAttachedAgents.size();
+			int sharedAgent = (totalAgentSize - userSpecificAgentCount);
+			limit = sharedAgent == 0 ? Long.MAX_VALUE
+				: (long) (configuredLimit / (((float) sharedAgent) / totalAgentSize));
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.ngrinder.extension.OnTestSamplingRunnable#sampling(org.ngrinder.service.ISingleConsole,
-     * org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService,
-     * net.grinder.statistics.ImmutableStatisticsSet, net.grinder.statistics.ImmutableStatisticsSet)
-     */
-    @Override
-    public void sampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService,
-                         ImmutableStatisticsSet intervalStatistics, ImmutableStatisticsSet cumulativeStatistics) {
-        LongIndex longIndex = singleConsole.getStatisticsIndexMap().getLongIndex(
-                StatisticsIndexMap.HTTP_PLUGIN_RESPONSE_LENGTH_KEY);
-        Long byteSize = intervalStatistics.getValue(longIndex);
-        if (byteSize > this.limit) {
-            if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
-                String message = String.format("Too much traffic on this test. Stop by force.\n"
-                        + "- LIMIT : %s - SENT :%s", UnitUtils.byteCountToDisplaySize(this.limit),
-                        UnitUtils.byteCountToDisplaySize(byteSize));
-                LOGGER.info(message);
-                LOGGER.info("Stop the test {} by force", perfTest.getTestIdentifier());
-                perfTestService.markStatusAndProgress(perfTest, Status.ABNORMAL_TESTING, message);
-            }
-        }
+		}
 
+		int getLimit() {
+			return this.config.getControllerProperties().getPropertyInt(
+				PROP_NETWORK_OVERFLOW_PERTEST_LIMIT, PROP_NETWORK_OVERFLOW_PERTEST_LIMIT_DEFAULT) * 1024 * 1024;
+		}
 
-    }
+		List<AgentInfo> getLocalAgents() {
+			return agentManagerService.getLocalAgents();
+		}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.ngrinder.extension.OnTestSamplingRunnable#endSampling(org.ngrinder.service.ISingleConsole
-     * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
-     */
-    @Override
-    public void endSampling(ISingleConsole singleConsole, PerfTest perfTest, IPerfTestService perfTestService) {
-    }
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see
+		 * org.ngrinder.extension.OnTestSamplingRunnable#sampling(org.ngrinder.service.ISingleConsole,
+		 * org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService,
+		 * net.grinder.statistics.ImmutableStatisticsSet, net.grinder.statistics.ImmutableStatisticsSet)
+		 */
+		@Override
+		public void sampling(ISingleConsole singleConsole, PerfTest perfTest,
+			IPerfTestService perfTestService, ImmutableStatisticsSet intervalStatistics,
+			ImmutableStatisticsSet cumulativeStatistics) {
+			LongIndex longIndex = singleConsole.getStatisticsIndexMap().getLongIndex(
+				StatisticsIndexMap.HTTP_PLUGIN_RESPONSE_LENGTH_KEY);
+			Long byteSize = intervalStatistics.getValue(longIndex);
+			if (byteSize > this.limit) {
+				if (perfTest.getStatus() != Status.ABNORMAL_TESTING) {
+					String message = String.format(
+						"Too much traffic on this test. Stop by force.\n"
+							+ "- LIMIT : %s - SENT :%s",
+						UnitUtils.byteCountToDisplaySize(this.limit),
+						UnitUtils.byteCountToDisplaySize(byteSize));
+					LOGGER.info(message);
+					LOGGER.info("Stop the test {} by force", perfTest.getTestIdentifier());
+					perfTestService.markStatusAndProgress(perfTest, Status.ABNORMAL_TESTING,
+						message);
+				}
+			}
+
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see
+		 * org.ngrinder.extension.OnTestSamplingRunnable#endSampling(org.ngrinder.service.ISingleConsole
+		 * , org.ngrinder.model.PerfTest, org.ngrinder.service.IPerfTestService)
+		 */
+		@Override
+		public void endSampling(ISingleConsole singleConsole, PerfTest perfTest,
+			IPerfTestService perfTestService) {
+		}
+	}
 
 }
